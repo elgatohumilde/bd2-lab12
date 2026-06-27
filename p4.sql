31d27855566dcb03bef16031e6c00b178f121e43
José Daniel Grayson <josedanielgrayson@proton.me>
2026-06-26 19:36:31 -0500

    feat(p4): finish all queries (except for choosing appropiate ks)

--- p4.sql
+++ p4.sql
@@ -27,13 +27,13 @@ declare
     retval pacientes%rowtype;
 begin
     -- TODO: choose appropriate partition values
-    create table pacientes_1p as select * from pacientes where fechanacimiento < '2020-01-01';
-    create table pacientes_2p as select * from pacientes where fechanacimiento >= '2020-01-01' and fechanacimiento < '2021-01-01';
-    create table pacientes_3p as select * from pacientes where fechanacimiento > '2021-01-01';
+    create temporary table pacientes_1p as select * from pacientes where fechanacimiento < '2020-01-01';
+    create temporary table pacientes_2p as select * from pacientes where fechanacimiento >= '2020-01-01' and fechanacimiento < '2021-01-01';
+    create temporary table pacientes_3p as select * from pacientes where fechanacimiento > '2021-01-01';
 
-    create table pacientes_1o as select * from pacientes_1p order by fechanacimiento;
-    create table pacientes_2o as select * from pacientes_2p order by fechanacimiento;
-    create table pacientes_3o as select * from pacientes_3p order by fechanacimiento;
+    create temporary table pacientes_1o as select * from pacientes_1p order by fechanacimiento;
+    create temporary table pacientes_2o as select * from pacientes_2p order by fechanacimiento;
+    create temporary table pacientes_3o as select * from pacientes_3p order by fechanacimiento;
 
     for retval in
         select * from pacientes_1o
@@ -57,9 +57,9 @@ returns setof varchar(50) as $$
 declare
     retval varchar(50);
 begin
-    create table pacientes_1p as select distinct ciudadorigen from pacientes_1;
-    create table pacientes_2p as select distinct ciudadorigen from pacientes_2;
-    create table pacientes_3p as select distinct ciudadorigen from pacientes_3;
+    create temporary table pacientes_1p as select distinct ciudadorigen from pacientes_1;
+    create temporary table pacientes_2p as select distinct ciudadorigen from pacientes_2;
+    create temporary table pacientes_3p as select distinct ciudadorigen from pacientes_3;
 
     for retval in
         select * from pacientes_1p
@@ -76,17 +76,46 @@ begin
 end;
 $$ language plpgsql;
 
-drop type if exists diagnostico_proms (
+drop type if exists diagnostico_proms;
+create type diagnostico_proms as (
     diagnostico varchar(50),
     promedad integer
 );
 
 -- select diagnostico, avg(edad) as promedad from atencionmedica group by diagnostico
-create or replace function query_4()
+create or replace function query_3()
 returns setof diagnostico_proms as $$
 declare
     retval diagnostico_proms;
 begin
+    -- TODO: choose appropriate partition values
+
+    -- Partición por rango según diagnostico
+    create temporary table atencionmedica_1p as select * from atencionmedica where diagnostico < 'H';
+    create temporary table atencionmedica_2p as select * from atencionmedica where diagnostico >= 'H' and diagnostico < 'P';
+    create temporary table atencionmedica_3p as select * from atencionmedica where diagnostico >= 'P';
+
+    -- Agrupamientos locales por rango de diagnóstico
+    create temporary table atencionmedica_1g as select diagnostico, avg(edad) as promedad from atencionmedica_1p group by diagnostico;
+    create temporary table atencionmedica_2g as select diagnostico, avg(edad) as promedad from atencionmedica_2p group by diagnostico;
+    create temporary table atencionmedica_3g as select diagnostico, avg(edad) as promedad from atencionmedica_3p group by diagnostico;
+
+    -- Como no hay diagnóstico presente en más de una partición a la vez,
+    -- basta con concatenar los agrupamientos locales.
+    for retval in
+        select * from atencionmedica_1g
+        union all
+        select * from atencionmedica_2g
+        union all
+        select * from atencionmedica_3g
+    loop
+        return next retval;
+    end loop;
+
+
+    drop table atencionmedica_1g, atencionmedica_2g, atencionmedica_3g;
+    drop table atencionmedica_1p, atencionmedica_2p, atencionmedica_3p;
+    return;
 end;
 $$ language plpgsql;
 
@@ -115,17 +144,17 @@ declare
     retval paciente_atencion;
 begin
     -- TODO: choose appropriate partition values
-    create table pacientes_1p as select * from pacientes where dni < '3';
-    create table pacientes_2p as select * from pacientes where dni >= '3' and dni < '6';
-    create table pacientes_3p as select * from pacientes where dni >= '6';
+    create temporary table pacientes_1p as select * from pacientes where dni < '3';
+    create temporary table pacientes_2p as select * from pacientes where dni >= '3' and dni < '6';
+    create temporary table pacientes_3p as select * from pacientes where dni >= '6';
 
-    create table atencionmedica_1p as select * from atencionmedica where dni < '3';
-    create table atencionmedica_2p as select * from atencionmedica where dni >= '3' and dni < '6';
-    create table atencionmedica_3p as select * from atencionmedica where dni >= '6';
+    create temporary table atencionmedica_1p as select * from atencionmedica where dni < '3';
+    create temporary table atencionmedica_2p as select * from atencionmedica where dni >= '3' and dni < '6';
+    create temporary table atencionmedica_3p as select * from atencionmedica where dni >= '6';
 
-    create table pacientes_1j as select * from pacientes_1p natural join atencionmedica_1p;
-    create table pacientes_2j as select * from pacientes_2p natural join atencionmedica_2p;
-    create table pacientes_3j as select * from pacientes_3p natural join atencionmedica_3p;
+    create temporary table pacientes_1j as select * from pacientes_1p natural join atencionmedica_1p;
+    create temporary table pacientes_2j as select * from pacientes_2p natural join atencionmedica_2p;
+    create temporary table pacientes_3j as select * from pacientes_3p natural join atencionmedica_3p;
 
     for retval in
         select * from pacientes_1j
@@ -145,5 +174,5 @@ $$ language plpgsql;
 
 select * from query_1();
 select * from query_2();
+select * from query_3();
 select * from query_4();
-
