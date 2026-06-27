e5900a22cb2268a399d3a4e07412c7cb0529c30e
José Daniel Grayson <josedanielgrayson@proton.me>
2026-06-26 19:20:32 -0500

    refactor(p4): use stored procedures for queries

--- p4.sql
+++ p4.sql
@@ -21,69 +21,129 @@ truncate table atencionmedica;
 \copy pacientes from 'pacientes.csv' delimiter ',' csv header;
 
 -- select * from pacientes order by fechanacimiento
+create or replace function query_1()
+returns setof pacientes as $$
+declare
+    retval pacientes%rowtype;
+begin
+    -- TODO: choose appropriate partition values
+    create table pacientes_1p as select * from pacientes where fechanacimiento < '2020-01-01';
+    create table pacientes_2p as select * from pacientes where fechanacimiento >= '2020-01-01' and fechanacimiento < '2021-01-01';
+    create table pacientes_3p as select * from pacientes where fechanacimiento > '2021-01-01';
 
--- TODO: select aproppriate partition values
-create table pacientes_1p as select * from pacientes where fechanacimiento < '2020-01-01';
-create table pacientes_2p as select * from pacientes where fechanacimiento >= '2020-01-01' and fechanacimiento < '2021-01-01';
-create table pacientes_3p as select * from pacientes where fechanacimiento > '2021-01-01';
+    create table pacientes_1o as select * from pacientes_1p order by fechanacimiento;
+    create table pacientes_2o as select * from pacientes_2p order by fechanacimiento;
+    create table pacientes_3o as select * from pacientes_3p order by fechanacimiento;
 
-create table pacientes_1o as select * from pacientes_1p order by fechanacimiento;
-create table pacientes_2o as select * from pacientes_2p order by fechanacimiento;
-create table pacientes_3o as select * from pacientes_3p order by fechanacimiento;
+    for retval in
+        select * from pacientes_1o
+        union all
+        select * from pacientes_2o
+        union all
+        select * from pacientes_3o
+    loop
+        return next retval;
+    end loop;
 
--- select * from pacientes_1o
--- union all
--- select * from pacientes_2o
--- union all
--- select * from pacientes_3o;
-
-drop table pacientes_1o;
-drop table pacientes_2o;
-drop table pacientes_3o;
-
-drop table pacientes_1p;
-drop table pacientes_2p;
-drop table pacientes_3p;
+    drop table pacientes_1o, pacientes_2o, pacientes_3o;
+    drop table pacientes_1p, pacientes_2p, pacientes_3p;
+    return;
+end;
+$$ language plpgsql;
 
 -- select distinct ciudadorigen from pacientes
-create table pacientes_1p as select distinct ciudadorigen from pacientes_1;
-create table pacientes_2p as select distinct ciudadorigen from pacientes_2;
-create table pacientes_3p as select distinct ciudadorigen from pacientes_3;
+create or replace function query_2()
+returns setof varchar(50) as $$
+declare
+    retval varchar(50);
+begin
+    create table pacientes_1p as select distinct ciudadorigen from pacientes_1;
+    create table pacientes_2p as select distinct ciudadorigen from pacientes_2;
+    create table pacientes_3p as select distinct ciudadorigen from pacientes_3;
 
--- select * from pacientes_1p
--- union all
--- select * from pacientes_2p
--- union all
--- select * from pacientes_3p;
+    for retval in
+        select * from pacientes_1p
+        union all
+        select * from pacientes_2p
+        union all
+        select * from pacientes_3p
+    loop
+        return next retval;
+    end loop;
 
-drop table pacientes_1p;
-drop table pacientes_2p;
-drop table pacientes_3p;
+    drop table pacientes_1p, pacientes_2p, pacientes_3p;
+    return;
+end;
+$$ language plpgsql;
+
+drop type if exists diagnostico_proms (
+    diagnostico varchar(50),
+    promedad integer
+);
 
 -- select diagnostico, avg(edad) as promedad from atencionmedica group by diagnostico
+create or replace function query_4()
+returns setof diagnostico_proms as $$
+declare
+    retval diagnostico_proms;
+begin
+end;
+$$ language plpgsql;
+
+drop type if exists paciente_atencion;
+create type paciente_atencion as (
+    dni char(8),
+    nombre varchar(50),
+    apellidos varchar(100),
+    fechanacimiento date,
+    sexo char(1),
+    ciudadorigen varchar(50),
+    codmedico integer,
+    ciudad varchar(50),
+    diagnostico varchar(50),
+    peso decimal(5, 2),
+    talla decimal(4, 2),
+    presionarterial varchar(10),
+    edad integer,
+    fechaatencion date
+);
 
 -- select * from pacientes natural join atencionmedica
--- TODO: choose proper values of k
-create table pacientes_1p as select * from pacientes where dni < '3';
-create table pacientes_2p as select * from pacientes where dni >= '3' and dni < '6';
-create table pacientes_3p as select * from pacientes where dni >= '6';
+create or replace function query_4()
+returns setof paciente_atencion as $$
+declare
+    retval paciente_atencion;
+begin
+    -- TODO: choose appropriate partition values
+    create table pacientes_1p as select * from pacientes where dni < '3';
+    create table pacientes_2p as select * from pacientes where dni >= '3' and dni < '6';
+    create table pacientes_3p as select * from pacientes where dni >= '6';
 
-create table atencionmedica_1p as select * from atencionmedica where dni < '3';
-create table atencionmedica_2p as select * from atencionmedica where dni >= '3' and dni < '6';
-create table atencionmedica_3p as select * from atencionmedica where dni >= '6';
+    create table atencionmedica_1p as select * from atencionmedica where dni < '3';
+    create table atencionmedica_2p as select * from atencionmedica where dni >= '3' and dni < '6';
+    create table atencionmedica_3p as select * from atencionmedica where dni >= '6';
 
-create table pacientes_1j as select * from pacientes_1p natural join atencionmedica_1p;
-create table pacientes_2j as select * from pacientes_2p natural join atencionmedica_2p;
-create table pacientes_3j as select * from pacientes_3p natural join atencionmedica_3p;
+    create table pacientes_1j as select * from pacientes_1p natural join atencionmedica_1p;
+    create table pacientes_2j as select * from pacientes_2p natural join atencionmedica_2p;
+    create table pacientes_3j as select * from pacientes_3p natural join atencionmedica_3p;
 
-select * from pacientes_1j
-union all
-select * from pacientes_2j
-union all
-select * from pacientes_3j;
+    for retval in
+        select * from pacientes_1j
+        union all
+        select * from pacientes_2j
+        union all
+        select * from pacientes_3j
+    loop
+        return next retval;
+    end loop;
 
+    drop table pacientes_1j, pacientes_2j, pacientes_3j;
+    drop table pacientes_1p, pacientes_2p, pacientes_3p;
+    return;
+end;
+$$ language plpgsql;
 
-drop table pacientes_1p;
-drop table pacientes_2p;
-drop table pacientes_3p;
+select * from query_1();
+select * from query_2();
+select * from query_4();
 
