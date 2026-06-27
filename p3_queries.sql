b3638264d94db53c8d12e095966477057580d360
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-26 22:04:20 -0500

    p3 queries

--- p3_queries.sql
+++ p3_queries.sql
@@ -1,2 +1,9 @@
 explain analyze
-select * from local_schema.atencionmedica;
+select diagnostico, count(*)
+from local_schema.atencionmedica
+group by diagnostico;
+
+explain analyze
+select *
+from local_schema.atencionmedica
+order by fechaatencion desc;
