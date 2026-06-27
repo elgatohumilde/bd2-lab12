c5b0142ae49539a4a6fc341ea9aa85255e362158
José Daniel Grayson <josedanielgrayson@proton.me>
2026-06-26 23:28:32 -0500

    add queries without explain analyze to p3

--- p3_queries.sql
+++ p3_queries.sql
@@ -1,3 +1,11 @@
+select diagnostico, count(*)
+from local_schema.atencionmedica
+group by diagnostico;
+
+select *
+from local_schema.atencionmedica
+order by fechaatencion desc;
+
 explain analyze
 select diagnostico, count(*)
 from local_schema.atencionmedica
